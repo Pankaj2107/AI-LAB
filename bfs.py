7a4d24092c8874b056f32b83449aa48a3a29e93c
Pankaj2107 <71484840+Pankaj2107@users.noreply.github.com>
2022-01-12 22:09:53 +0530

    Add files via upload

---
+++ bfs.py
@@ -0,0 +1,23 @@
+def bfs(v):
+    visited.add(v)
+    queue.append(v)
+    while queue:
+        s = queue.pop(0)
+        print(s, end=" ")
+        for neighbour in graph[s]:
+            if neighbour not in visited:
+                visited.add(neighbour)
+                queue.append(neighbour)
+
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+visited = set()
+queue = []
+bfs("A")
